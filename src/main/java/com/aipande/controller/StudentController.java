294cc1e0acab47507290884c0dcc6df2f7d161ec
aipande <18963981950@163.com>
2020-08-04 17:10:42 +0800

    update StudentController

--- src/main/java/com/aipande/controller/StudentController.java
+++ src/main/java/com/aipande/controller/StudentController.java
@@ -1,9 +1,21 @@
 package com.aipande.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Author: WangPan
  * Description:学生控制类
  * Date-Of-Create:2020/7/31-21:05
  */
+@Controller
 public class StudentController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        System.out.println("hello");
+        return "hello SpringBoot!";
+    }
 }
